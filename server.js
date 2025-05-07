8c8e904a456670412e9e33fee15be78e9a5e5411
Test-Blitzy <test4@pratikmekhe.com>
2025-05-07 16:41:19 +0530

    File: server.js

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const express = require('express');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-
 const app = express();
 
-// Define route for root path
+// Root path handler
 app.get('/', (req, res) => {
   res.status(200).type('text/plain').send('Hello, World!\n');
 });
 
-// Define route for evening path
+// Evening path handler
 app.get('/evening', (req, res) => {
   res.status(200).type('text/plain').send('Good evening');
 });
 
-// Start the server
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
